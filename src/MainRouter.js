4fbda8e6541a9bd2006f6cd4c47a85d1840e7f35
youssef samih <youssef.samih97@gmail.com>
2019-07-14 17:27:51 +0100

    add private Route and use it in mainRouter

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -7,6 +7,7 @@ import Menu from './core/Menu';
 import Profile from './user/Profile';
 import Users from './user/user';
 import EditProfile from './user/EditProfile';
+import PrivateRoute from './auth/PrivateRoute';
 
 const MainRouter = () => (
     <div>
@@ -16,8 +17,8 @@ const MainRouter = () => (
             <Route exact path="/users" component={Users}/>
             <Route exact path="/signup" component={Signup}/>
             <Route exact path="/signin" component={Signin}/>
-            <Route exact path="/user/:userId" component={Profile}/>
-            <Route exact path="/user/edit/:userId" component={EditProfile}/>
+            <PrivateRoute exact path="/user/:userId" component={Profile}/>
+            <PrivateRoute exact path="/user/edit/:userId" component={EditProfile}/>
         </Switch>
     </div>
 )
