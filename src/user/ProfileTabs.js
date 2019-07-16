3a6175f9357f22162e796b8a8b13182fd16cf29d
youssef samih <youssef.samih97@gmail.com>
2019-07-16 17:57:28 +0100

    profileTabs add commponent

--- src/user/ProfileTabs.js
+++ src/user/ProfileTabs.js
@@ -5,7 +5,6 @@ import DefautProfile from '../images/avatar.gif'
 export default class ProfileTabs extends Component {
     render() {
         const {following, followers} = this.props
-        console.log(following);
         
         return (
             <div>
@@ -14,24 +13,40 @@ export default class ProfileTabs extends Component {
                         <h3 className="text-primary">Followers</h3>
                         {followers.map((person, i) =>  (
                             <div key={i}>
-                                <div className="row">
                                     <div>
                                         <Link to={`/user/${person._id}`}>
                                             <img src=
                                             {`localhost:8080/user/photo.${person._id}`} alt={person.name} 
-                                            className="float-left mr-2" height="30px" onError={i =>  (i.target.src = `${DefautProfile}`)} />
+                                            className="float-left mr-2" height="30px" width="30px" style={{ borderRadius: "50%", border: "1px solid black" }} onError={i =>  (i.target.src = `${DefautProfile}`)} />
                                             <div>
-                                                <h3> {person.name} </h3>
+                                                <p className="lead"> {person.name} </p>
                                             </div>
                                         </Link>
-                                        <p>
-                                            {person.about}
-                                        </p>
                                     </div>
-                                </div>
                             </div>
                             )
                         )}
+                    </div>
+                    <div className="col-md-4">
+                        <h3 className="text-primary">Following</h3>
+                        {following.map((person, i) =>  (
+                            <div key={i}>
+                                    <div>
+                                        <Link to={`/user/${person._id}`}>
+                                            <img src=
+                                            {`localhost:8080/user/photo.${person._id}`} alt={person.name} 
+                                            className="float-left mr-2" height="30px" width="30px" style={{ borderRadius: "50%", border: "1px solid black" }} onError={i =>  (i.target.src = `${DefautProfile}`)} />
+                                            <div>
+                                            <p className="lead"> {person.name} </p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                            </div>
+                            )
+                        )}
+                    </div>
+                    <div className="col-md-4">
+                        <h3 className="text-primary">Posts</h3>
                     </div>
                 </div>
             </div>
