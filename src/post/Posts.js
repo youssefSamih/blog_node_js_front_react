b13e0f9f82bc891629d70cde57e80bb664c7a1b3
youssef samih <youssef.samih97@gmail.com>
2019-07-23 01:07:17 +0100

    pagination

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -2,24 +2,40 @@ import React, {Component} from 'react'
 import { list } from './apiPost'
 import DefautPost from '../images/defaultimgblog.jpg'
 import {Link} from 'react-router-dom'
+import { number } from 'prop-types';
 require('dotenv').config()
 
 class Posts extends Component {
     constructor() {
         super()
         this.state = {
-            posts: []
+            posts: [],
+            page: 1
         }
     }
 
     componentDidMount() {
-        list().then(data => {
+        this.loadPosts(this.state.page)
+    }
+    
+    loadPosts = page => {
+        list(page).then(data => {
             if(data.error){
                 console.log(data.error)
             } else {
                 this.setState({posts: data})
             }
         })
+    }
+
+    loadMore = number => {
+        this.setState({ page: this.state.page + number })
+        this.loadPosts(this.state.page + number)
+    }
+
+    loadLess = number => {
+        this.setState({ page: this.state.page - number })
+        this.loadPosts(this.state.page - number)
     }
 
     renderPosts = posts => {
@@ -50,11 +66,21 @@ class Posts extends Component {
     }
 
     render() {
-        const { posts } = this.state
+        const { posts, page } = this.state
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5"> {!posts.length ? "Loading..." : "Recent Posts"} </h2>
                 {this.renderPosts(posts)}
+                {page > 1 ? (
+                    <button className="btn btn-raised btn-warning mr-5 mt-5 mb-5" onClick={() => this.loadLess(1)}>Previous ({this.state.page - 1})</button>
+                ) : (
+                    ""
+                )}
+                {posts.length ? (
+                    <button className="btn btn-raised btn-success mt-5 mb-5" onClick={() => this.loadMore(1)}>
+                        Next ({page + 1})
+                    </button>
+                ) : ("")}
             </div>
         )
     }
